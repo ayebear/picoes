4d8f558bbb946adebe79665762f4df8b7af74a4a
Eric Hebert <ayebear@gmail.com>
2016-06-30 22:20:09 -0400

    Added tests for components

--- test/test-entities.js
+++ test/test-entities.js
@@ -4,15 +4,61 @@ let assert = require('chai').assert
 
 describe('World', function() {
 	describe('component()', function () {
-		let world = new es.World()
-
-		world.component('position', function(x, y) {
-			this.x = x
-			this.y = y
-		})
-
 		it('should define a component', function () {
+			let world = new es.World()
+			world.component('position', function(x, y) {
+				this.x = x
+				this.y = y
+			})
+			let ent = world.entity().set('position', 1, 2)
 			assert('position' in world.components)
+			assert(Object.keys(world.components).length == 1)
+			assert(ent.has('position'))
+			assert(ent.get('position').x === 1)
+			assert(ent.get('position').y === 2)
+		})
+		it('should define an object component', function () {
+			let world = new es.World()
+			world.component('position', {
+				x: 0,
+				y: 0
+			})
+			let ent = world.entity().merge('position', {
+				x: 1,
+				y: 2
+			})
+			let ent2 = world.entity().merge('position', {
+				x: 1
+			})
+			assert('position' in world.components)
+			assert(Object.keys(world.components).length == 1)
+			assert(ent.has('position'))
+			assert(ent.get('position').x === 1)
+			assert(ent.get('position').y === 2)
+			assert(ent2.has('position'))
+			assert(ent2.get('position').x === 1)
+			assert(ent2.get('position').y === 0)
+		})
+		it('should define an empty component', function () {
+			let world = new es.World()
+			world.component('position')
+			let ent = world.entity().merge('position', {
+				x: 1
+			})
+			assert('position' in world.components)
+			assert(Object.keys(world.components).length == 1)
+			assert(ent.has('position'))
+			assert(ent.get('position').x === 1)
+			assert(!('y' in ent.get('position')))
+		})
+	})
+
+	describe('system()', function() {
+		it('should define a system', function() {
+			let world = new es.World()
+			world.component('position')
+			world.system(['position'], class {})
+			assert(world.systems.length == 1)
 		})
 	})
 })
