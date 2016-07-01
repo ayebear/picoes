4fa2951f926fc5f2481dd3201166b82da7e1c726
Eric Hebert <ayebear@gmail.com>
2016-06-30 22:40:49 -0400

    Implemented entity merge() Entity set() now handles special component registration cases (such as components as objects) Implemented clone() function

--- src/picoes.js
+++ src/picoes.js
@@ -6,12 +6,31 @@ function isObject(obj) {
 	return typeof obj === 'object'
 }
 
+function isPrimitive(obj) {
+	return (typeof obj === 'boolean') || (typeof obj === 'number') || (typeof obj === 'string')
+}
+
+// TODO: Figure out a better solution for component templates and prototypes
+function clone(obj) {
+	if (isPrimitive(obj)) {
+		return obj
+	}
+	return JSON.parse(JSON.stringify(obj))
+}
+
 class Entity {
 	constructor(world) {
 		this.world = world
 		this.data = {}
 	}
 
+	// Checks if a component exists
+	has(component) {
+		return component in this.data
+	}
+
+	// Returns a component by name
+	// ent.get('position')
 	get(component) {
 		if (!this.has(component)) {
 			this.set(component)
@@ -22,21 +41,31 @@ class Entity {
 	// Adds or overwrites a new component
 	// ent.set('position', 1, 2)
 	set(component, ...args) {
-		let objType = this.world.components[component]
-		this.data[component] = new objType(...args)
+		let compTemplate = this.world.components[component]
+		if (isFunction(compTemplate)) {
+			this.data[component] = new compTemplate(...args)
+		} else if (isObject(compTemplate)) {
+			this.data[component] = clone(compTemplate)
+		} else {
+			this.data[component] = {}
+		}
 		return this
 	}
 
 	// Updates data from existing component (and creates a new one first if necessary)
 	// ent.merge('position', {x: 1, y: 2})
 	merge(component, data) {
-
+		let comp = this.get(component)
+		for (let key in data) {
+			comp[key] = data[key]
+		}
 		return this
 	}
 
 	// Removes a component from the entity (no effect when it doesn't exist)
 	// ent.remove('position')
 	// Also can remove all components: ent.remove()
+	// TODO: Maybe split into removeAll, seems dangerous
 	remove(component) {
 		if (component === undefined) {
 			// Remove all
@@ -46,11 +75,6 @@ class Entity {
 			delete this.data[component]
 		}
 		return this
-	}
-
-	// Checks if a component exists
-	has(component) {
-		return component in this.data
 	}
 
 	// Serializes entire entity to JSON
