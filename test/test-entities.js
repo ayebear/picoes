468f835669916440dc9e8a4efb46934fdd10c657
Eric Hebert <ayebear@gmail.com>
2016-06-30 23:13:03 -0400

    Added tests for entities Various component operations

--- test/test-entities.js
+++ test/test-entities.js
@@ -4,9 +4,9 @@ let assert = require('chai').assert
 
 describe('World', function() {
 	describe('component()', function () {
-		it('should define a component', function () {
+		it('define a component', function () {
 			let world = new es.World()
-			world.component('position', function(x, y) {
+			world.component('position', function(x = 0, y = 0) {
 				this.x = x
 				this.y = y
 			})
@@ -17,7 +17,7 @@ describe('World', function() {
 			assert(ent.get('position').x === 1)
 			assert(ent.get('position').y === 2)
 		})
-		it('should define an object component', function () {
+		it('define an object component', function () {
 			let world = new es.World()
 			world.component('position', {
 				x: 0,
@@ -39,7 +39,7 @@ describe('World', function() {
 			assert(ent2.get('position').x === 1)
 			assert(ent2.get('position').y === 0)
 		})
-		it('should define an empty component', function () {
+		it('define an empty component', function () {
 			let world = new es.World()
 			world.component('position')
 			let ent = world.entity().merge('position', {
@@ -54,11 +54,103 @@ describe('World', function() {
 	})
 
 	describe('system()', function() {
-		it('should define a system', function() {
+		it('define a system', function() {
 			let world = new es.World()
 			world.component('position')
 			world.system(['position'], class {})
 			assert(world.systems.length == 1)
+		})
+	})
+
+	describe('entity()', function() {
+		it('create an entity', function() {
+			let world = new es.World()
+			world.component('position')
+			let ent = world.entity()
+			assert(Object.keys(world.entities).length == 1)
+		})
+		it('support components', function() {
+			let world = new es.World()
+			world.component('position', function(x = 0, y = 0) {
+				this.x = x
+				this.y = y
+			})
+			world.component('object', {
+				val: 0,
+				test: 1
+			})
+			world.component('empty')
+			let ent = world.entity()
+			ent.set('position', 5)
+			assert(Object.keys(ent.data).length == 1)
+			assert(ent.get('position').x === 5)
+			assert(ent.get('position').y === 0)
+
+			ent.merge('position', {y: 3})
+			assert(Object.keys(ent.data).length == 1)
+			assert(ent.get('position').x === 5)
+			assert(ent.get('position').y === 3)
+
+			ent.merge('object', {val: 50})
+			assert(Object.keys(ent.data).length == 2)
+			assert(ent.get('object').val === 50)
+			assert(ent.get('object').test === 1)
+
+			ent.merge('empty', {testing: 100})
+			assert(Object.keys(ent.data).length == 3)
+			assert(ent.get('empty').testing === 100)
+		})
+		it('remove components', function() {
+			let world = new es.World()
+			world.component('position')
+			world.component('velocity')
+			let ent = world.entity().set('position').set('velocity')
+			assert(Object.keys(ent.data).length == 2)
+			assert(ent.has('position'))
+			assert(ent.has('velocity'))
+
+			ent.remove('position')
+			assert(Object.keys(ent.data).length == 1)
+			assert(!ent.has('position'))
+			assert(ent.has('velocity'))
+
+			ent.remove('velocity')
+			assert(Object.keys(ent.data).length == 0)
+			assert(!ent.has('position'))
+			assert(!ent.has('velocity'))
+
+			ent.set('position').set('velocity')
+			assert(Object.keys(ent.data).length == 2)
+			assert(ent.has('position'))
+			assert(ent.has('velocity'))
+			ent.remove()
+			assert(Object.keys(ent.data).length == 0)
+			assert(!ent.has('position'))
+			assert(!ent.has('velocity'))
+		})
+		it('serialize components', function() {
+			let world = new es.World()
+			world.component('position')
+			let ent = world.entity().merge('position', {x: 4, y: 6})
+
+			let data = JSON.parse(ent.toJson())
+			assert(data)
+			assert(data.position)
+			assert(data.position.x === 4)
+			assert(data.position.y === 6)
+		})
+		it('deserialize components', function() {
+			let world = new es.World()
+			world.component('position')
+			let ent = world.entity()
+			assert(Object.keys(ent.data).length == 0)
+
+			ent.fromJson('{"position": {"x": 4, "y": 6}}')
+			assert(ent.has('position'))
+			assert(Object.keys(ent.data).length == 1)
+			assert(ent.get('position'))
+			assert(ent.get('position').x === 4)
+			assert(ent.get('position').y === 6)
 		})
 	})
 })
